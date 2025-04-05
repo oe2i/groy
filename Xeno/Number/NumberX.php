42840bf30d75343f915e8fabada7b8d78a908bf2
AO™ <anthony@osawere.com>
2025-04-05 22:13:00 +0100

    Create NumberX.php

---
+++ Xeno/Number/NumberX.php
@@ -0,0 +1,56 @@
+<?php //*** NumberX ~ class » Groy™ Library © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xero\Number;
+
+class NumberX
+{
+	// • === even » is even?
+	public static function even($number)
+	{
+		return ($number % 2 == 0);
+	}
+
+
+
+	// • === evenBetween »
+	public static function evenBetween(int $start, int $end): array
+	{
+		$evens = [];
+		for ($i = $start; $i <= $end; $i++) {
+			if ($i % 2 === 0) {
+				$evens[] = $i;
+			}
+		}
+		return $evens;
+	}
+
+
+
+	// • === odd » is odd?
+	public static function odd($number)
+	{
+		return (!self::even($number));
+	}
+
+
+
+	// • === oddBetween »
+	public static function oddBetween(int $start, int $end): array
+	{
+		$odds = [];
+		for ($i = $start; $i <= $end; $i++) {
+			if ($i % 2 !== 0) {
+				$odds[] = $i;
+			}
+		}
+		return $odds;
+	}
+
+
+
+	// • === format »
+	public static function format($number, $decimal = 0, $separator = ',', $pointer = '.')
+	{
+		return number_format($number, $decimal, $pointer, $separator);
+	}
+} //> end of class ~ NumberX
