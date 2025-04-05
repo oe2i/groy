ea281f842351f3f39b0847c6761d71b7f1f923a9
AO™ <anthony@osawere.com>
2025-04-05 13:39:29 +0100

    Create alias.php

---
+++ alias.php
@@ -0,0 +1,6 @@
+<?php //*** Alias ~ function » Groy™ Library © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+return [
+	'NumberX' => Groy\Core\Number\NumberX::class,
+	'RandomX' => Groy\Core\Data\RandomX::class,
+	'RequestX' => Groy\Core\Http\RequestX::class,
+]; //> end of function ~ Alias
