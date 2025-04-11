f2fb917fe9d554f8d1edd2870c3e8686b1aecf7b
AO™ <anthony@osawere.com>
2025-04-11 17:21:22 +0100

    Create InFileX.php

---
+++ Xeno/File/InFileX.php
@@ -0,0 +1,11 @@
+<?php //*** InFileX ~ class » Groy™ Library © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\File;
+
+use Groy\Xeno\Data\RandomX;
+
+class InFileX
+{
+	// • === ... »
+
+} //> end of class ~ InFileX
