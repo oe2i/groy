a3d628e71e64bb5fef874c304b99d2e5da479885
AO™ <anthony@osawere.com>
2025-04-11 17:21:11 +0100

    Create PageX.php

---
+++ Xeno/Theme/PageX.php
@@ -0,0 +1,12 @@
+<?php //*** PageX ~ class » Groy™ Library © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\Theme;
+
+class PageX
+{
+	// • === as »
+	public static function as($file)
+	{
+		return ThemeX::as('page', $file);
+	}
+} //> end of class ~ PageX
