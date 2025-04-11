d82921265dd8cc4324bccf2053e209d14200f3ff
AO™ <anthony@osawere.com>
2025-04-11 20:21:39 +0100

    Create CanX.php

---
+++ Spine/Core/CanX.php
@@ -0,0 +1,33 @@
+<?php //*** CanX ~ class » Groy™ Library © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Spine\Core;
+
+class CanX
+{
+	// • === iterate » boolean
+	public static function iterate(&$var)
+	{
+		return is_iterable($var);
+	}
+
+
+
+	// • === string » boolean
+	public static function string(&$var)
+	{
+		if (!blank($var)) {
+
+			// ~ is scalar [string, integer, float, or boolean]
+			if (is_scalar($var)) {
+				return true;
+			}
+
+			// ~ is an object that implements __toString
+			if (is_object($var) && method_exists($var, '__toString')) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+} //> end of class ~ CanX
