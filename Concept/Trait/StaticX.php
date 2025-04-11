0a38b76f2307c889f4a5e4db0ccb8f52dfad3b7c
AO™ <anthony@osawere.com>
2025-04-11 10:06:07 +0100

    Create StaticX.php

---
+++ Concept/Trait/StaticX.php
@@ -0,0 +1,43 @@
+<?php //*** StaticX ~ trait » Groy™ Library © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Concept\Trait;
+
+trait StaticX
+{
+	// • === property »
+	public static function property(string|array $prop, $key = null)
+	{
+		// ➝ get property
+		if (is_string($prop) && isset(self::${$prop})) {
+
+			if (!empty($key)) {
+
+				if (is_object(self::${$prop}) && isset(self::${$prop}->$key)) {
+					return self::${$prop}->$key;
+				}
+
+				if (is_array(self::${$prop}) && isset(self::${$prop}[$key])) {
+					return self::${$prop}[$key];
+				}
+
+				return false;
+			}
+
+			return self::${$prop};
+		}
+
+
+		// ➝ set property
+		if (is_array($prop)) {
+
+			foreach ($prop as $property => $value) {
+				self::${$property} = $value;
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+} //> end of trait ~ StaticX
