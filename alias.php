2d00dca7c80797ea8108b007cea99b2cc7b270d8
AO™ <anthony@osawere.com>
2025-04-11 10:05:57 +0100

    Update alias.php

--- alias.php
+++ alias.php
@@ -1,6 +1,7 @@
 <?php //*** Alias ~ function » Groy™ Library © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
 return [
-	'NumberX' => Groy\Core\Number\NumberX::class,
-	'RandomX' => Groy\Core\Data\RandomX::class,
-	'RequestX' => Groy\Core\Http\RequestX::class,
+	'DebugX' => Groy\Spine\DebugX::class,
+	// 'NumberX' => Groy\Core\Number\NumberX::class,
+	// 'RandomX' => Groy\Core\Data\RandomX::class,
+	// 'RequestX' => Groy\Core\Http\RequestX::class,
 ]; //> end of function ~ Alias
