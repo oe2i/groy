1a595ce3724391787c2d68ac16b9bda986821006
AO™ <anthony@osawere.com>
2025-04-14 12:02:46 +0100

    Create ValueX.php

---
+++ Xeno/Data/Array/ValueX.php
@@ -0,0 +1,56 @@
+<?php //*** ValueX ~ class » Groy™ Library © April, 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\Data\Array;
+
+use Groy\Xeno\Data\ArrayX;
+
+class ValueX
+{
+	// • === is → is array and value in? » boolean
+	public static function is($array, $value)
+	{
+		return (ArrayX::has($array) && in_array($value, $array));
+	}
+
+
+
+
+	// • === isNot → is array but $value not in? » boolean
+	public static function isNot($array, $value)
+	{
+		return (ArrayX::has($array) && !in_array($value, $array));
+	}
+
+
+
+	// • === first »
+	public static function first($array)
+	{
+		if (ArrayX::has($array)) {
+			return reset($array);
+		}
+		return false;
+	}
+
+
+
+	// • === last »
+	public static function last($array)
+	{
+		if (ArrayX::has($array)) {
+			return end($array);
+		}
+		return false;
+	}
+
+
+
+	// • === list → array values » boolean, numerically indexed
+	public static function list($array)
+	{
+		if (ArrayX::has($array)) {
+			return array_values($array);
+		}
+		return false;
+	}
+} //> end of class ~ ValueX
