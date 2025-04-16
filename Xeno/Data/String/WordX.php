71f78db0fbbedff75155ac0f9357f48d8d3e40ee
AO™ <anthony@osawere.com>
2025-04-16 13:01:07 +0100

    Create WordX.php

---
+++ Xeno/Data/String/WordX.php
@@ -0,0 +1,41 @@
+<?php //*** WordX ~ class » Groy™ Library © April, 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\Data\String;
+
+use Groy\Xeno\Data\StringX;
+use Illuminate\Support\Str;
+
+class WordX
+{
+	// • === firstWord »
+	public static function first($string)
+	{
+		if (!StringX::has($string)) {
+			return false;
+		}
+		return Str::words($string, 1);
+	}
+
+
+
+	// • === count »
+	public static function count($string)
+	{
+		if (!StringX::has($string)) {
+			return false;
+		}
+		return str_word_count($string);
+	}
+
+
+
+	// • === limit » limit number of words
+	public static function limit($string, $noOfWords)
+	{
+		if (!StringX::has($string) || !is_numeric($noOfWords)) {
+			return false;
+		}
+
+		return Str::words($string, $noOfWords);
+	}
+} //> end of class ~ WordX
