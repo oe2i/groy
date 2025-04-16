865f663c644a389c7848870a5d7a93784b068f15
AO™ <anthony@osawere.com>
2025-04-16 13:30:17 +0100

    Update HasX.php

--- Xeno/Data/String/HasX.php
+++ Xeno/Data/String/HasX.php
@@ -7,52 +7,92 @@ use Illuminate\Support\Str;
 
 class HasX
 {
-	// • === number »
-	public static function number($var)
+	// • === something »
+	public static function something($string)
 	{
-		return preg_match('/\d/', $var) === 1;
+		return StringX::length($string) > 0;
+	}
+
+
+
+	// • === nothing »
+	public static function nothing($string)
+	{
+		return (self::something($string) === false);
+	}
+
+
+
+	// • === number »
+	public static function number($string)
+	{
+		if (self::nothing($string)) {
+			return false;
+		}
+		return preg_match('/\d/', $string) === 1;
 	}
 
 
 
 	// • === character »
-	public static function character($var, $ignoreSpace = true)
+	public static function character($string, $ignoreSpace = true)
 	{
-		if ($ignoreSpace) {
-			return preg_match('/[^a-zA-Z0-9\s]/', $var) === 1;
+		if (self::nothing($string)) {
+			return false;
 		}
-		return preg_match('/[^a-zA-Z0-9]/', $var) === 1;
+
+		if ($ignoreSpace) {
+			return preg_match('/[^a-zA-Z0-9\s]/', $string) === 1;
+		}
+
+		return preg_match('/[^a-zA-Z0-9]/', $string) === 1;
 	}
 
 
 
 	// • === letter »
-	public static function letter($var)
+	public static function letter($string)
 	{
-		return preg_match('/[a-zA-Z]/', $var) === 1;
+		if (self::nothing($string)) {
+			return false;
+		}
+
+		return preg_match('/[a-zA-Z]/', $string) === 1;
 	}
 
 
 
 	// • === space »
-	public static function space($var)
+	public static function space($string)
 	{
-		return (strpos(trim($var), ' ') !== false);
+		if (self::nothing($string)) {
+			return false;
+		}
+
+		return (strpos(trim($string), ' ') !== false);
 	}
 
 
 
 	// • === newline »
-	public static function newline($var)
+	public static function newline($string)
 	{
-		return (strpos($var, "\n") !== false || strpos($var, "\r\n") !== false || strpos($var, "\r") !== false);
+		if (self::nothing($string)) {
+			return false;
+		}
+
+		return (strpos($string, "\n") !== false || strpos($string, "\r\n") !== false || strpos($string, "\r") !== false);
 	}
 
 
 
 	// • === paragraph »;
-	public static function paragraph($var)
+	public static function paragraph($string)
 	{
-		return (preg_match('/(\R){2,}/', $var));
+		if (self::nothing($string)) {
+			return false;
+		}
+
+		return (preg_match('/(\R){2,}/', $string));
 	}
 } //> end of class ~ HasX
