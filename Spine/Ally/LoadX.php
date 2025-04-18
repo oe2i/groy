5ed34da4ee6cd0c5ee2ffc48283c91a22e62be94
AO™ <anthony@osawere.com>
2025-04-18 12:14:05 +0100

    Update LoadX.php

--- Spine/Ally/LoadX.php
+++ Spine/Ally/LoadX.php
@@ -1,33 +1,47 @@
-<?php //*** LoadX ~ class » Groy™ Library © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+<?php //*** LoadX ~ class » Groy™ Library © April, 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
 namespace Groy\Spine\Ally;
 
 use Groy\Spine\Core\PathX;
-use Groy\Spine\Ally\BladeX;
+use Groy\Xeno\Data\StringX;
+use Groy\Spine\Core\DebugX;
+// use Groy\Spine\Ally\BladeX;
 
 class LoadX
 {
+	// • === file »
+	public static function file($file, $label = 'LoadX'){
+
+		if (is_string($file)) {
+			$file = [$file];
+		}
+
+		foreach ($file as $key => $filename) {
+			if (is_file($filename)) {
+				unset($file[$key]);
+				$o = require $filename;
+			}
+		}
+
+		$count = count($file);
+		if(empty($count)){
+			return $o;
+		}
+
+		return DebugX::oversight('LoadX', StringX::plural('file', $count).' unavailable', $file);
+	}
+
+
+
 	// • === route »
 	public static function route(string $path = '', ?string $to = null)
 	{
 		$file = PathX::router($path);
 		$file = !empty($file) ? $file : $path;
-
 		if (is_string($file) && is_dir($file)) {
-			// TODO: make sure path is not ending with '//'
 			$file = glob($file . PathX::property('DS') . '*.php');
 		}
-
-		if (is_string($file) && is_file($file)) {
-			return require $file;
-		}
-
-		if (is_array($file)) {
-			foreach ($file as $filename) {
-				if (is_file($filename)) {
-					require $filename;
-				}
-			}
-		}
+		return self::file($file);
 	}
 
 
