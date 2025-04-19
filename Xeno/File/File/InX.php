63c6be4b559e3731059cd1c05dd4f4c0120c2571
AO™ <anthony@osawere.com>
2025-04-19 02:05:37 +0100

    Create InX.php

---
+++ Xeno/File/File/InX.php
@@ -0,0 +1,11 @@
+<?php //*** InX ~ class » Groy™ Library © April, 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\File\File;
+
+use Groy\Spine\Core\PathX;
+
+class InX
+{
+
+
+} //> end of class ~ InX
