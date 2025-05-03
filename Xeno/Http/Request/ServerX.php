b89a1ba55c43b6342b48baf868beae7f939aff43
AO™ <anthony@osawere.com>
2025-05-03 19:21:58 +0100

    Create ServerX.php

---
+++ Xeno/Http/Request/ServerX.php
@@ -0,0 +1,8 @@
+<?php //*** ServerX ~ class » Groy™ Library © May, 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\Http\Request;
+
+class ServerX
+{
+
+} //> end of class ~ ServerX
