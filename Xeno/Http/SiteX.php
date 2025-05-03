f66157e7b26e3305dd25ed6036871a45e790babf
AO™ <anthony@osawere.com>
2025-05-03 19:22:06 +0100

    Create SiteX.php

---
+++ Xeno/Http/SiteX.php
@@ -0,0 +1,34 @@
+<?php //*** SiteX ~ class » Groy™ Library © May, 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\Http;
+
+use Illuminate\Support\Facades\Route;
+use Groy\Xeno\Skin\PageX;
+use Groy\Xeno\Data\StringX;
+
+class SiteX
+{
+	// • === view »
+	public static function view($route, $view = null, $name = null)
+	{
+		if ($route === 'index') {
+			$route = '/';
+			$view = 'index';
+		}
+
+		if (!$view) {
+			if (StringX::has()->onlyLetter($route)) {
+				$view = $route;
+			}
+		}
+
+		if (!$name) {
+			if (StringX::has()->onlyLetter($view)) {
+				$name = 'site.' . $view;
+			}
+		}
+
+		return Route::view($route, PageX::site($view))->name($name);
+	}
+
+} //> end of class ~ SiteX
