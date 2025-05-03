11c111c964d5a43ad1f1fa0323633977e863cd08
AO™ <anthony@osawere.com>
2025-05-03 20:17:21 +0100

    Update SiteX.php

--- Xeno/Http/SiteX.php
+++ Xeno/Http/SiteX.php
@@ -3,6 +3,8 @@
 namespace Groy\Xeno\Http;
 
 use Illuminate\Support\Facades\Route;
+use Groy\Xeno\Vine\BladeX;
+use Groy\Xeno\Skin\ThemeX;
 use Groy\Xeno\Skin\PageX;
 use Groy\Xeno\Data\StringX;
 
@@ -15,6 +17,18 @@ class SiteX
 			$route = '/';
 			$view = 'index';
 		}
+		if ($route === 'welcome') {
+			if (!RouteX::valid('index')) {
+				$route = '/';
+			}
+
+			if (!self::valid('welcome')) {
+				$view = 'index';
+			} elseif (!$view) {
+				$view = 'welcome';
+			}
+		}
+
 
 		if (!$view) {
 			if (StringX::has()->onlyLetter($route)) {
@@ -29,6 +43,16 @@ class SiteX
 		}
 
 		return Route::view($route, PageX::site($view))->name($name);
+	}
+
+
+
+
+	// • === valid »
+	public static function valid($view)
+	{
+		$blade = ThemeX::prepare('site', $view);
+		return BladeX::is($blade);
 	}
 
 } //> end of class ~ SiteX
