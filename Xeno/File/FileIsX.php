dbabe1fb26f19f86e70b388c4178d7f4559c728b
AO™ <anthony@osawere.com>
2025-05-05 04:25:13 +0100

    Create FileIsX.php

---
+++ Xeno/File/FileIsX.php
@@ -0,0 +1,48 @@
+<?php //*** FileIsX ~ class » Groy™ Library © May, 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\File;
+
+use Illuminate\Support\Facades\View;
+use Groy\Xeno\Core\PathX;
+use Groy\Xeno\Core\EnvX;
+use Groy\Xeno\Enum\Image as ImageEnumX;
+use Groy\Xeno\Data\Array\ValueX as ArrValueX;
+use Groy\Xeno\Data\String\EndX as StrEndX;
+use Groy\Xeno\Data\String\BeginX as StrBeginX;
+
+class FileIsX
+{
+	// • === invoke »
+	public function __invoke($file)
+	{
+		return FileX::exist($file);
+	}
+
+
+
+
+	// • === public »
+	public static function public($file)
+	{
+		return is_file(PathX::public($file));
+	}
+
+
+
+
+	// • === blade »
+	public static function blade($blade)
+	{
+		return View::exists($blade);
+	}
+
+
+
+
+	// • === logo »
+	public static function logo($logo = null)
+	{
+		return FileInX::theme(FileOrganizeX::logo($logo));
+	}
+
+} //> end of class ~ FileIsX
