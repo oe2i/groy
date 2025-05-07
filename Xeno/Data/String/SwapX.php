ec3fb18e105f51deaa24b472bb6c42bd0c0b2d9d
AO™ <anthony@osawere.com>
2025-05-07 13:12:26 +0100

    Update SwapX.php

--- Xeno/Data/String/SwapX.php
+++ Xeno/Data/String/SwapX.php
@@ -7,37 +7,68 @@ use Groy\Xeno\Data\StringX;
 class SwapX
 {
 	// • === text »
-	public static function text($string, $needle, $substitute, $occurrence = 'all', $case = false)
+	public static function text($string, $search, $substitute, $occurrence = 'all', $case = false)
 	{
-		$occurrence = strtolower($occurrence);
+		$occurrence = !empty($occurrence) ? strtolower($occurrence) : $occurrence;
 
-		if (!StringX::in($string, $needle, $case)) {
+		if (!StringX::contain($string, $search, $case)) {
 			return $string;
 		}
 
 		if ($occurrence === 'all') {
 			return $case
-				? str_replace($needle, $substitute, $string)
-				: str_ireplace($needle, $substitute, $string);
+				? str_replace($search, $substitute, $string)
+				: str_ireplace($search, $substitute, $string);
 		}
 
 		switch ($occurrence) {
 			case 'first':
-				$pos = $case ? strpos($string, $needle) : stripos($string, $needle);
+				$pos = $case ? strpos($string, $search) : stripos($string, $search);
 				break;
 			case 'last':
-				$pos = $case ? strrpos($string, $needle) : strripos($string, $needle);
+				$pos = $case ? strrpos($string, $search) : strripos($string, $search);
 				break;
 			default:
 				return $string;
 		}
 
 		if ($pos !== false) {
-			return substr_replace($string, $substitute, $pos, strlen($needle));
+			return substr_replace($string, $substitute, $pos, strlen($search));
 		}
 
 		return $string;
 	}
+
+
+
+
+
+	// • === all → all occurrence »
+	public static function all($string, $search, $substitute = '', $case = false)
+	{
+		return self::text($string, $search, $substitute, 'all', $case);
+	}
+
+
+
+
+
+	// • === first → first occurrence »
+	public static function first($string, $search, $substitute = '', $case = false)
+	{
+		return self::text($string, $search, $substitute, 'first', $case);
+	}
+
+
+
+
+	// • === last → last occurrence »
+	public static function last($string, $search, $substitute = '', $case = false)
+	{
+		return self::text($string, $search, $substitute, 'last', $case);
+	}
+
+
 
 
 
@@ -46,6 +77,8 @@ class SwapX
 	{
 		return self::text($string, DIRECTORY_SEPARATOR, $substitute, $occurrence);
 	}
+
+
 
 
 
@@ -58,6 +91,7 @@ class SwapX
 
 
 
+
 	// • === hyphen »
 	public static function hyphen($string, $substitute, $occurrence = 'all')
 	{
@@ -66,46 +100,28 @@ class SwapX
 
 
 
-	// • === all → all occurrence »
-	public static function all($string, $needle, $substitute = '', $case = false)
-	{
-		return self::text($string, $needle, $substitute, 'all', $case);
-	}
-
-
-
-	// • === first → first occurrence »
-	public static function first($string, $needle, $substitute = '', $case = false)
-	{
-		return self::text($string, $needle, $substitute, 'first', $case);
-	}
-
-
-
-	// • === last → last occurrence »
-	public static function last($string, $needle, $substitute = '', $case = false)
-	{
-		return self::text($string, $needle, $substitute, 'last', $case);
-	}
-
 
 
 	// • === space → replace space with character & vice-versa »
-	public static function space($string, $needle, $inverse = false)
+	public static function space($string, $search, $inverse = false)
 	{
 		if ($inverse) {
-			return StringX::in($string, $needle) ? self::text($string, $needle, ' ') : $string;
+			return StringX::contain($string, $search) ? self::text($string, $search, ' ') : $string;
 		}
-		return StringX::contain($string, 'space') ? self::text($string, ' ', $needle) : $string;
+
+		return StringX::contain($string, 'space') ? self::text($string, ' ', $search) : $string;
 	}
+
+
 
 
 
 	// • === withSpace → replace character with space »
-	public static function withSpace($string, $needle)
+	public static function withSpace($string, $search)
 	{
-		return self::space($string, $needle, true);
+		return self::space($string, $search, true);
 	}
+
 
 
 
@@ -122,28 +138,28 @@ class SwapX
 
 
 
+
 	// • === beginIf » swap if string begins with needle
-	public static function beginIf($string, $needle, $substitute, $case = false)
+	public static function beginIf($string, $search, $substitute, $case = false)
 	{
-		if (BeginX::with($string, $needle, $case)) {
-			$string = self::first($string, $needle, $substitute, $case);
+		if (BeginX::with($string, $search, $case)) {
+			$string = self::first($string, $search, $substitute, $case);
 		}
+
 		return $string;
 	}
+
 
 
 
 
 	// • === endIf » swap if string ends with needle
-	public static function endIf($string, $needle, $substitute, $case = false)
+	public static function endIf($string, $search, $substitute, $case = false)
 	{
-		if (EndX::with($string, $needle, $case)) {
-			$string = self::last($string, $needle, $substitute, $case);
+		if (EndX::with($string, $search, $case)) {
+			$string = self::last($string, $search, $substitute, $case);
 		}
+
 		return $string;
 	}
-
-
-
-
 } //> end of class ~ SwapX
