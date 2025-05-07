53916d17d6a2ed20eff317cc0ec33bd2dd7ca0fb
AO™ <anthony@osawere.com>
2025-05-07 12:57:12 +0100

    Update WordX.php

--- Xeno/Data/String/WordX.php
+++ Xeno/Data/String/WordX.php
@@ -2,36 +2,43 @@
 
 namespace Groy\Xeno\Data\String;
 
+use Groy\Xeno\Data\StringX;
 use Illuminate\Support\Str;
 
 class WordX
 {
-	// • === firstWord »
+	// • === first »
 	public static function first($string)
 	{
-		if (HasX::nothing($string)) {
+		if (!StringX::valid($string)) {
 			return false;
 		}
+
 		return Str::words($string, 1);
 	}
+
+
 
 
 
 	// • === count »
 	public static function count($string, $character = null)
 	{
-		if (HasX::nothing($string)) {
+		if (!StringX::valid($string)) {
 			return false;
 		}
+
 		return str_word_count($string, $character);
 	}
+
+
 
 
 
 	// • === limit » limit number of words
 	public static function limit($string, $noOfWords)
 	{
-		if (HasX::nothing($string) || !is_numeric($noOfWords)) {
+		if (!StringX::valid($string) || !is_numeric($noOfWords)) {
 			return false;
 		}
 
