e419b79c3c3b85bfa357169d662467b3c55e6f86
AO™ <anthony@osawere.com>
2025-05-12 13:42:00 +0100

    Update SpaceX.php

--- Xeno/Data/String/SpaceX.php
+++ Xeno/Data/String/SpaceX.php
@@ -2,12 +2,34 @@
 
 namespace Groy\Xeno\Data\String;
 
+use Groy\Xeno\Trait\FluentX;
 use Groy\Xeno\Data\StringX;
 
 class SpaceX
 {
-	// • === no → remove any space »
-	public static function no($string)
+	// ◇ trait
+	use FluentX;
+
+
+
+
+
+	// ◇ === has »
+	public static function has($string)
+	{
+		if (!StringX::valid($string)) {
+			return false;
+		}
+
+		return (strpos(trim($string), ' ') !== false);
+	}
+
+
+
+
+
+	// ◇ === remove » remove any space
+	public static function remove($string)
 	{
 		return SwapX::all($string, ' ', '');
 	}
@@ -16,7 +38,7 @@ class SpaceX
 
 
 
-	// • === single » allow single spaces
+	// ◇ === single » allow single spaces
 	public static function single($string)
 	{
 		if (!StringX::valid($string)) {
@@ -30,7 +52,7 @@ class SpaceX
 
 
 
-	// • === upper »
+	// ◇ === upper » space from uppercase
 	public static function upper($string)
 	{
 		return CaseX::upperToSpace($string);
@@ -40,9 +62,104 @@ class SpaceX
 
 
 
-	// • === lower »
-	public static function lowerToSpace($string)
+	// ◇ === lower » space from lowercase
+	public static function lower($string)
 	{
 		return CaseX::lowerToSpace($string);
 	}
+
+
+
+
+
+	// ◇ === to »
+	public static function to($string, $substitute, $occurrence = 'all')
+	{
+		return SwapX::text($string, ' ', $substitute, $occurrence);
+	}
+
+
+
+
+
+	// ◇ === toDot »
+	public static function toDot($string, $occurrence = 'all')
+	{
+		return self::to($string, '.', $occurrence);
+	}
+
+
+
+
+
+	// ◇ === toPS »
+	public static function toPS($string, $occurrence = 'all')
+	{
+		return self::to($string, '/', $occurrence);
+	}
+
+
+
+
+
+	// ◇ === toPS »
+	public static function toDS($string, $occurrence = 'all')
+	{
+		return self::to($string, DIRECTORY_SEPARATOR, $occurrence);
+	}
+
+
+
+
+
+	// ◇ === toHyphen »
+	public static function toHyphen($string, $occurrence = 'all')
+	{
+		return self::to($string, '-', $occurrence);
+	}
+
+
+
+
+
+	// ◇ === toUnderscore »
+	public static function toUnderscore($string, $occurrence = 'all')
+	{
+		return self::to($string, '_', $occurrence);
+	}
+
+
+
+
+
+	// ◇ === toUpper »
+	public static function toUpper($string)
+	{
+		$firstChar = StringX::first($string);
+		$string = StripX::nth($string, 1);
+		if (StringX::length($string) > 0) {
+			return $firstChar . lcfirst(ucwords($string));
+		}
+		return $firstChar;
+	}
+
+
+
+
+
+	// ◇ === toLower »
+	public static function toLower($string)
+	{
+		$firstChar = StringX::first($string);
+		$string = StripX::nth($string, 1);
+		if (StringX::length($string) > 0) {
+			$words = explode(' ', $string);
+			$words = array_map(function ($word) {
+				return lcfirst($word);
+			}, $words);
+			return $firstChar . implode(' ', $words);
+		}
+		return $firstChar;
+	}
+
 } //> end of class ~ SpaceX
