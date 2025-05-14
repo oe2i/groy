8eefe32c3d8a9c196f26e3a2362a149fdcda4290
AO™ <anthony@osawere.com>
2025-05-14 18:07:43 +0100

    Created TimeX

---
+++ Xeno/Moment/TimeX.php
@@ -0,0 +1,16 @@
+<?php //*** TimeX ~ class » Groy™ Library © May, 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Groy\Xeno\Moment;
+
+class TimeX
+{
+	// • property
+	private static bool $init = false;
+
+
+	// • === init »
+	public static function init($groy = null)
+	{
+	}
+
+} //> end of class ~ TimeX
